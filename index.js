1d02eb3b53d6a3dd58276ca659f4725ac7e66b35
Ricardo Vazquez <rickyrabv@gmail.com>
2023-10-06 00:05:57 +0200

    Bonus work done

--- index.js
+++ index.js
@@ -43,5 +43,69 @@ for (let a = 0; a < nameUppercased.length; a++) {
     }
     else if (nameUppercased[a] === nameReversed[a]){
         console.log("What?! You both have the same name?");
+        continue;
     }
 }
+//Bonus 
+const longText = `Donec vel lectus sit amet mauris laoreet volutpat in nec lorem. Maecenas ultrices ut diam eget auctor. 
+Suspendisse congue sed odio commodo facilisis. Proin congue scelerisque condimentum. 
+Integer semper, urna commodo pellentesque cursus, augue lorem tincidunt sapien, molestie placerat diam neque sed lorem. 
+Proin velit ex, tempus et sollicitudin ac, ullamcorper porta lorem. 
+Aliquam viverra nunc est, in lacinia purus facilisis vitae. 
+Nulla non pulvinar eros. Ut ac ullamcorper nisl. 
+Cras sagittis bibendum turpis sed egestas. 
+Etiam pellentesque mi augue, id sodales purus tincidunt sed. 
+Donec vitae fermentum purus. Duis quis nisl et ipsum pharetra accumsan eget elementum elit. 
+Duis porttitor viverra lacus vel finibus. Ut tristique pellentesque ex, eget ultricies odio cursus eu. 
+Nulla nec tellus in lectus pharetra fringilla. 
+Duis laoreet, urna lobortis cursus euismod, risus odio lobortis nunc, interdum euismod quam erat a ex. 
+Morbi tincidunt non metus at tempor. Quisque interdum nisl mi. Nulla fringilla purus quis turpis fermentum, nec congue dui rhoncus. 
+Suspendisse at est bibendum, hendrerit lacus lacinia, elementum dolor. Nullam nec finibus urna, ac dignissim ligula. 
+Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris vel nulla eget tellus auctor tempus. 
+Morbi vehicula dolor quam, rhoncus viverra ipsum consectetur vitae. Ut eu nunc neque. In tincidunt ut nisl ac elementum. 
+Proin id tellus vel quam finibus varius eget sed odio. Nam ut laoreet nibh. Nulla at quam sit amet velit pretium blandit. 
+Curabitur lacinia mauris non nisi commodo auctor. Sed condimentum commodo tempus. Vestibulum sit amet ornare ipsum, id suscipit odio. 
+Pellentesque blandit tortor neque, quis vestibulum ligula sollicitudin vitae.`
+
+let wordCount = 0
+for (b = 0; b < longText.length; b++) {
+    if (longText[b] === " ") {
+        wordCount += 1;
+    }
+}
+let fixedWordCount = wordCount + 1;
+console.log(fixedWordCount);
+
+
+function checkPalindrom (phraseToCheck){
+    let palindromeReversed = ""
+    for (let c = phraseToCheck.length - 1; c >= 0; c--) {
+        if (phraseToCheck[c] === " ") {
+            continue;
+        }
+        else {
+            palindromeReversed += phraseToCheck[c];
+        }
+    }
+    console.log(palindromeReversed);
+
+    let palindromeNoSpace = "";
+    for (d = 0; d < phraseToCheck.length; d++) {
+        if (phraseToCheck[d] === " ") {
+            continue;
+        }
+        else (palindromeNoSpace += phraseToCheck[d]);
+    }
+    console.log(palindromeNoSpace);
+
+    if (palindromeNoSpace === palindromeReversed) {
+        console.log("This is a Palindrome");
+    }
+    else {
+        console.log("This is not a Palindrome");
+    }
+}
+
+phrase1 = "taco cat"
+
+checkPalindrom (phrase1)
